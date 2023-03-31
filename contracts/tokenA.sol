af97dea167661a1b22105b46e74aa4147b8f7c0f
Faytey7 <Faithroberts708@gmail.com>
2023-03-31 10:54:00 +0100

    major fixes on logic

--- contracts/tokenA.sol
+++ contracts/tokenA.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenB is ERC20{
+contract TokenA is ERC20{
     address owner;
-    constructor() ERC20("TokenB", "TKB"){
+    constructor() ERC20("TokenA", "TKA"){
         owner = msg.sender;
         _mint(owner, 100000000 * (10 **18));
     }
-
 
     function withdraw(uint256 _amount) external returns(bool success){
         require(msg.sender == owner, "You're not the owner");
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,56 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner, profile1, profile3] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const TokenA = await ethers.getContractFactory("TokenA");
+  const tokenA = await TokenA.deploy();
+  await tokenA.deployed();
+  console.log(`Token A contract deployed to ${tokenA.address}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const TokenB = await ethers.getContractFactory("TokenB");
+  const tokenB = await TokenB.deploy();
+  await tokenB.deployed();
+  console.log(`Token B contract deployed to ${tokenB.address}`);
 
-  await lock.deployed();
+  const balance = await tokenB.balanceOf(tokenB.address);
+  console.log("balance of tokenB is ", balance);
 
+  await tokenB.transfer(profile1.address, 2000);
+
+  const balance1 = await tokenB.balanceOf(profile1.address);
+  console.log("balance of user is ", balance1);
+  console.log("success");
+
+  const LaunchPad = await ethers.getContractFactory("LaunchPad");
+  const launchpad = await LaunchPad.deploy(owner.address);
+  await launchpad.deployed();
+  console.log(`Launch Pad contract deployed to ${launchpad.address}`);
+
+  const amount = ethers.utils.parseEther("0.01");
+
+  const approval = tokenB
+    .connect(profile1.address)
+    .approve(launchpad.address, 2000);
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "launchpad allowance of token b is",
+    await tokenB
+      .connect(profile1.address)
+      .allowance(profile1.address, launchpad.address)
   );
+
+  const createPad = await launchpad
+    .connect(profile1)
+    .createLaunchPad(
+      "Faith's Funds",
+      tokenA.address,
+      tokenB.address,
+      60,
+      300,
+      100,
+      100,
+      { value: amount }
+    );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
